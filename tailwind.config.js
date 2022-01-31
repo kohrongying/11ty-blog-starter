2eb031634f505ed7f5c35bc61a3288e98e2d6a86
rongying <kohrongying@gmail.com>
2022-01-31 11:23:51 +0800

    [ry] remove redundant  config.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,64 +1,6 @@
 module.exports = {
   darkMode: 'class',
   content: ['./src/**/*.md', './src/_includes/**/*.liquid'],
-  theme: {
-    extend: {
-      typography: (theme) => ({
-        dark: {
-          css: {
-            color: 'white',
-            a: {
-              color: '#9ECE6A',
-              '&:hover': {
-                color: '#9ECE6A',
-              },
-            },
-            h1: {
-              color: 'white',
-            },
-            h2: {
-              color: 'white',
-            },
-            h3: {
-              color: 'white',
-            },
-            h4: {
-              color: 'white',
-            },
-            h5: {
-              color: 'white',
-            },
-            h6: {
-              color: 'white',
-            },
-            th: {
-              color: 'white',
-            },
-            strong: {
-              color: 'white',
-            },
-            'blockquote p': {
-              color: 'white',
-            },
-            code: {
-              color: 'white',
-            },
-            figcaption: {
-              color: theme('colors.gray.500'),
-            },
-            '::selection': {
-              backgroundColor: 'white',
-            },
-          },
-        }
-      })
-    },
-  },
-  variants: {
-    extend: {
-      typography: ['dark']
-    }
-  },
   plugins: [
     require('@tailwindcss/typography')
   ],
